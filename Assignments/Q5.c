78beb7f78a76740576bc4f30073ef2ab8668066e
DKC195 <Dhirajkc195@gmail.com>
2021-06-06 18:59:09 +0545

    Updated Style

--- Assignments/Q5.c
+++ Assignments/Q5.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int v,w,x,y,z;
+  int v,w,x,z;
 
   printf("Enter a Number:\n");
   scanf("%d", &x);
 
-  y=x/2;
-  y=y+1;
-
-  for (w=1; w<y; w++) {
+  for (w=1; w<x/2; w++) {
     z=x%(w+1);
-
-    if (z==0) {
-      break;
-    }
+    if (z==0) break;
   }
 
-  if (z==0) {
-    printf("The number %d is not a prime number.\n", x);
-  }
-  else {
-    printf("The number %d is a prime number.\n", x);
-  }
+  if (z==0) printf("The number %d is not a prime number.\n", x);
+  else printf("The number %d is a prime number.\n", x);
 
   return 0;
 }
