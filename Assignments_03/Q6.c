33efebd12259577002f9748698da1cf5977429da
DKC195 <Dhirajkc195@gmail.com>
2021-06-07 01:57:54 +0545

    Modified Q19 and Q6, Added Q7

--- Assignments_03/Q6.c
+++ Assignments_03/Q6.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  int x,y;
   for (int i=1; i<6; i++) {
     for (int j=0; j<10; j++) {
       for (int k=0; k<10; k++) {
-        x=i*i*i+j*j*j+k*k*k;
-        y=i*100+j*10+k;
-        if (x==y) printf("The number %d is Armstrong Number\n",x);
+        if (i*i*i+j*j*j+k*k*k==i*100+j*10+k) printf("The number %d is Armstrong Number\n",i*100+j*10+k);
         else {}
       }
     }
---
+++ Assignments_03/Q7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int n,x=0,y=1,z;
+  printf("Enter a natural number n:\n");
+  scanf("%d", &n);
+  if (n<0) printf("The number is negative.\n");
+  else {
+    printf("The Fibonacci Series is: %d, %d, ",x, y);
+    for (int i=0; i<n; i++) {
+      z=x+y;
+      x=y;
+      y=z;
+      printf("%d,", z);
+    }
+  }
+  return 0;
+}
