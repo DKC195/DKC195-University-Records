43a37530fc1deffc70f4c9ee192c44a1308196b6
DKC195 <Dhirajkc195@gmail.com>
2021-06-06 14:32:32 +0545

    Renamed Files and Updated Q5

--- Assignments/Q5.c
+++ Assignments/Q5.c
@@ -2,23 +2,33 @@
 
 int main()
 {
-  int x,y,z;
+  int v,w,x,y,z;
 
   printf("Enter a Number:\n");
-  scanf("%d\n", &x);
+  scanf("%d", &x);
 
   y=x/2;
   y=y+1;
 
-  printf("Enter a Number:\n");
-  scanf("%d\n", &x);
-  for (x=0; x<y; x++)
+  for (w=1; w<y; w++)
   {
-    z=
-    if ()
+    z=x%(w+1);
+
+    if (z==0)
     {
-      
+      break;
     }
+  }
+
+  printf("%d\n", z);
+
+  if (z==0)
+  {
+    printf("The number %d is not a prime number.\n", x);
+  }
+  else
+  {
+    printf("The number %d is a prime number.\n", x);
   }
   return 0;
 }
