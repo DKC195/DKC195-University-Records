e8321870813ff31632679fa7a9d3764603239a87
DKC195 <Dhirajkc195@gmail.com>
2021-06-06 22:48:07 +0545

    Updated

--- Assignments/Q3.c
+++ Assignments/Q3.c
@@ -2,9 +2,7 @@
 
 int main() {
   int x,y;
-  for (x=1; x<101; x++) {
-    y=y+x*x*x;
-  }
+  for (x=1; x<101; x++) y=y+x*x*x;
   printf("The sum of the cubes of the numbers is %d\n",y);
   return 0;
 }
