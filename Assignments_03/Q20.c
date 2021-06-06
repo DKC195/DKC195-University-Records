9b9644e2013b74a4f76a6d9dc4e32fe27e48c203
DKC195 <Dhirajkc195@gmail.com>
2021-06-07 02:28:07 +0545

    Updated Q19 and Q20, Added Q9

--- Assignments_03/Q20.c
+++ Assignments_03/Q20.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
   int n,x,s;
   printf("Enter the natural number n:\n");
   scanf("%d", &n);
-  if (n<1) {
-    printf("The Number is not natural.\n");
-    exit(0);
-  }
-  for (int i=1; i<n+1; i++) {
-    x=i*i;
-    printf("The square of %d is %d.\n", i, x);
-    s=s+x;
+  if (n<1) printf("The Number is not natural.\n");
+  else {
+    for (int i=1; i<n+1; i++) {
+      x=i*i;
+      printf("The square of %d is %d.\n", i, x);
+      s=s+x;
+    }
   }
   printf("The sum of square is %d.\n", x);
   return 0;
