d133e6196caeae433f7139aa5551615a1d0c78b4
DKC195 <Dhirajkc195@gmail.com>
2021-06-07 22:26:46 +0545

    Fixed Q9, renamed files and added Q10

--- Assignments_03/Q09.c
+++ Assignments_03/Q09.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  int x=1,y,b=0;
+  int x=1,y=0,b=0;
   for (int c=1; c<256; c++) {
     y=c;
     do {
       b=b+x*(y%2);
       y=y/2;
       x=x*10;
-    } while(y>0);
+    } while (y>0);
     printf("The ASCII code %c for is %8d \n", c, b);
+    b=0;
+    x=1;
   }
   return 0;
 }
