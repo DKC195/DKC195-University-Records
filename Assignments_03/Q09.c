f5dc0d437680e88eef30d1f4901fdf72abc30dea
DKC195 <Dhirajkc195@gmail.com>
2021-06-08 12:40:20 +0545

    Fixed Q9

--- Assignments_03/Q09.c
+++ Assignments_03/Q09.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  int x=1,y=0,b=0;
-  for (int c=1; c<256; c++) {
-    y=c;
-    do {
-      b=b+x*(y%2);
-      y=y/2;
-      x=x*10;
-    } while (y>0);
-    printf("The ASCII code %c for is %8d \n", c, b);
-    b=0;
-    x=1;
-  }
+  for (int c=1; c<256; c++) printf("The ASCII code %c for is %3d \n", c, c);
   return 0;
 }
