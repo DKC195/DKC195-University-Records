b4454a492107d007457aa9a9388d279903faba3a
DKC195 <Dhirajkc195@gmail.com>
2021-06-10 02:06:10 +0545

    Completed A03 Q16

--- Assignment_03/Q16.c
+++ Assignment_03/Q16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  int n,m,r1,r2,p1,p2,h,i=1;
+  int n,m;
   printf("Enter the first number:\n");
   scanf("%d", &n);
   printf("Enter the second number:\n");
   scanf(" %d", &m);
-  while(p1!=p2) {
+  int a1,a2,a;
+  for (int i=1; i<n+1; i++) {
+    a1=n%i;
+    a2=m%i;
+    if (a1==0 && a2==0) a=i;
+  }
+  printf("The HCF is %d.\n",a);
+  int p1,r;
+  for (int i=1; i<m+1; i++) {
     p1=n*i;
-    p2=m*i;
-    i++;
+    r=p1%m;
+    if (r==0) break;
   }
-  for (i=1; i<n+1; i++) {
-    r1=n%i;
-    r2=m%i;
-    if (r1==0 && r2==0) h=i;
-  }
-  printf("The HCF is %d.\nAnd the LCM is %d.", h, p1);
+  printf("And The LCM is %d.", p1);
   return 0;
 }
