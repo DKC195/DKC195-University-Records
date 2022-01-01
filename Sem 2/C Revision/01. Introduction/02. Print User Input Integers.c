d803d4ec87c9142d266dc712e0a1f4f9a56c9fd8
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-01-01 22:22:59 +0545

    Just Updating

---
+++ Sem 2/C Revision/01. Introduction/02. Print User Input Integers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int i;
+  printf("Enter an Integer: ");
+  scanf("%d",&i);
+  printf("\nYou entered %d.",i);
+  return 0;
+}
---
+++ Sem 2/C++/Lab Sheet 1/Q2.c
@@ -0,0 +1,9 @@
+// Write a program to find the position of the character 'C' in the sentence "idea without execution is worthless" using pointer and string.
+#include <stdio.h>
+
+int main()
+{
+  char sth[40] = "idea without execution is worthless";
+  printf("%s", sth);
+  return 0;
+}
