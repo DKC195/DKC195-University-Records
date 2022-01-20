88f0f8b86f57a59e388d7773fdb94b465466b07e
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-01-20 23:34:31 +0545

    Lab Sheet 2 Questions

---
+++ Sem 2/C++/Lab Sheet 2/Q5.cpp
@@ -0,0 +1,1 @@
+// Assume that employee will have to pay 10 percent income tax to the government. Ask user to enter the employee salary. Use inline function to display the net payment to the employee by the company.
