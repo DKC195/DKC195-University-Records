bb3fd426f54f553019a232ea7a7db92bf8372e0e
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-02-15 22:36:51 +0545

    Update Q4.cpp

--- Sem 2/C++/Lab Sheet 2/Q4.cpp
+++ Sem 2/C++/Lab Sheet 2/Q4.cpp
@@ -1,1 +1,32 @@
 // Write a function that passes two temperatures by reference and sets the larger of the two numbers to a value entered by user by using return by reference.
+#include <iostream>
+using namespace std;
+
+void temp(int *x, int *y)
+{
+  int a = *x, b = *y;
+  if (a < b)
+  {
+    *x = b;
+    *y = a;
+  }
+}
+
+int main()
+{
+  int x, y;
+  int *a, *b;
+  a = &x;
+  b = &y;
+
+  cout << "Enter 1st Number: ";
+  cin >> x;
+  cout << "Enter 2nd Number: ";
+  cin >> y;
+
+  temp(a, b);
+
+  cout << "Largest Value: " << x;
+
+  return 0;
+}
