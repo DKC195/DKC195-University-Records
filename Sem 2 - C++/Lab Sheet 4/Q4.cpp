29dc98d58718edb0dcafe61f07938b0e6b881222
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-03-10 08:58:09 +0545

    Update Q4.cpp

--- Sem 2 - C++/Lab Sheet 4/Q4.cpp
+++ Sem 2 - C++/Lab Sheet 4/Q4.cpp
@@ -1,3 +1,48 @@
-/* Assume that one constructor initializes data member say num_vehicle, hour, and rate. There 
-should be a 10% discount if num_vehicle exceeds 10. Display the total charge. Use two objects 
+/* Assume that one constructor initializes data member say num_vehicle, hour, and rate. There
+should be a 10% discount if num_vehicle exceeds 10. Display the total charge. Use two objects
 and show a bit-by-bit copy of one object to another (make your own copy constructor). */
+#include <iostream>
+using namespace std;
+
+class Mem
+{
+private:
+  int *num_vehicle = new int, *hour = new int, *rate = new int;
+
+public:
+  Mem();
+  ~Mem();
+
+  int display()
+  {
+    int dis = (*num_vehicle > 10) ? 10 : 0;
+    cout << "Total Charge: " << *num_vehicle * *rate * (100 - dis) << endl;
+    return 0;
+  }
+};
+
+Mem::Mem()
+{
+  cout << "Register Your Vehicle" << endl
+       << "Enter No. of Vechicles: ";
+  cin >> *num_vehicle;
+  cout << "Enter No. of Hours Parked: ";
+  cin >> *hour;
+  cout << "Enter Rates: ";
+  cin >> *rate;
+}
+
+Mem::~Mem()
+{
+  delete num_vehicle;
+  delete hour;
+  delete rate;
+}
+
+int main()
+{
+  Mem V1;
+  Mem V2 = V1;
+  V2.display();
+  return 0;
+}
