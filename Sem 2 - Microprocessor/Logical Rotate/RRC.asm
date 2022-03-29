f9f8da5f895578fdefd4a05d481b45275d56d6ef
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-03-29 23:32:00 +0545

    Create RAL.asm

--- Sem 2 - Microprocessor/Logical Rotate/RRC.asm
+++ Sem 2 - Microprocessor/Logical Rotate/RRC.asm
@@ -1,12 +1,12 @@
-      LXI H, 08A0H	; SETTING UP - RRC - ADDRESS = 08A0H
+      LXI H, 08A0H	; SETTING UP - RAR - ADDRESS = 08A0H
       MVI M, 70H		; VAL(08A0H) = 30H
       MVI C, 08H    ; COUNT C = 08H
 
-      LXI H, 08A0H  ; RRC BEGIN - ADDRESS = 08A0H "INPUT = 70 00 00 00 00 00 00 00 00"
+      LXI H, 08A0H  ; RAR BEGIN - ADDRESS = 08A0H "INPUT = 70 00 00 00 00 00 00 00 00"
 LOOP: MOV A, M      ; A = VAL(08A0H) "= 70H"
       INX H         ;
-      RRC           ; RRC "A = E0H"
+      RAR           ; RAR "A = E0H"
       MOV M, A      ;
       DCR C         ;
       JNZ LOOP      ;
-      HLT           ; "OUTPUT = 70 38	1C 0E	07 83	C1 E0	70"
+      HLT           ; "OUTPUT = 70 38	1C 0E	07 03	81 C0	E0 70"
